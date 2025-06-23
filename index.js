c68e15020669c4e08a475eb8e02391668329bb97
Akash187 <kumarakashseth007@gmail.com>
2025-06-24 00:26:41 +0530

    added code for test case for 'empty string, one and two numbers'

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const sum = (numString) => {}
+const sum = (numString) => {
+	if (!numString) return 0
+
+	const numStringArr = numString.split(',')
+	const numArr = numStringArr.map(Number)
+
+	return numArr.reduce((total, curr) => (total += curr), 0)
+}
 
 module.exports = { sum }
