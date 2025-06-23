0611583685dae1b2b8d1a0cc94753383021d0769
Akash187 <kumarakashseth007@gmail.com>
2025-06-24 00:23:25 +0530

    added test case for 'empty string, one and two numbers'

--- index.test.js
+++ index.test.js
@@ -1,5 +1,19 @@
 const { sum } = require('./index')
 
-test('sum function should exist', () => {
-	expect(typeof sum).toBe('function')
+describe('Test sum function', () => {
+	test('should function exist', () => {
+		expect(typeof sum).toBe('function')
+	})
+
+	test('Empty string', () => {
+		expect(sum('')).toBe(0)
+	})
+
+	test('Single number string', () => {
+		expect(sum('1')).toBe(1)
+	})
+
+	test('Two number string', () => {
+		expect(sum('1,2')).toBe(3)
+	})
 })
