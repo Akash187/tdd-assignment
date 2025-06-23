c71da5e2903a5ac4cbc62bd009b8893aec057c9f
Akash187 <kumarakashseth007@gmail.com>
2025-06-24 00:41:15 +0530

    added code to pass test 'Handle newline between numbers'

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const sum = (numString) => {
 	if (!numString) return 0
 
-	const numStringArr = numString.split(',')
-	const numArr = numStringArr.map(Number)
+	const numArr = []
+
+	// parse full string and find all the numbers including negative numbers
+	let curr = ''
+	const regex = /[0-9-]/
+	for (let i = 0; i < numString.length; i++) {
+		if (regex.test(numString[i])) {
+			curr += numString[i]
+		} else {
+			if (Number(curr)) {
+				numArr.push(Number(curr))
+			}
+			curr = ''
+		}
+	}
+
+	if (Number(curr)) {
+		numArr.push(Number(curr))
+	}
 
 	return numArr.reduce((total, curr) => (total += curr), 0)
 }
